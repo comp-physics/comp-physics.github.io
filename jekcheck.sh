5506a8fd26ce8fe0368bf8b4a92a9e687fc1a022
Spencer Bryngelson <sbryngelson@gmail.com>
2023-05-20 14:14:38 -0400

    some cleanup

--- jekcheck.sh
+++ jekcheck.sh
@@ -1,4 +1,7 @@
-bundle exec jekyll build; bundle exec htmlproofer ./_site --ignore-status-codes 999,403,301,302 --ignore-missing-alt=true --enforce-https=false --ignore-urls https://f.vimeocdn.com --ignore-missing-alt true
+bundle exec jekyll build; bundle exec htmlproofer ./_site --ignore-status-codes 999,403,301,302 --ignore-urls https://f.vimeocdn.com
+# --enforce-https=false 
+# --ignore-missing-alt=true 
+# --ignore-missing-alt true
 
 # bundle exec jekyll build; bundle exec htmlproofer ./_site --alt-ignore '/.*/' --http_status_ignore='999,403,301,302' --assume-extension
 
